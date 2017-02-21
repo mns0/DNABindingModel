a68327154b00d8012030e9d1944f5376fe0b537d
mns0 <shankla2@illinois.edu>
2017-02-21 09:59:54 -0600

    ci

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import helper as hp
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.path as mplPath
+from matplotlib.path import Path
 import matplotlib.patches as mpatches
 
 def inSpiral(coor):
@@ -25,18 +25,23 @@ def drawSpiral(coor):
     y1 = 30*np.exp(0.14*t)*np.sin(t) + 15
     x2 = 18*np.exp(0.15*t)*np.cos(t) - 7
     y2 = 18*np.exp(0.15*t)*np.sin(t) + 12
-    if inSpiral(coor):
+    plt.plot(x1,y1,x2,y2)
+    plt.scatter(x,y,s=100)
+    xx = np.hstack((x2,x1[::-1]))
+    yy = np.hstack((y2,y1[::-1]))
+    z1 = zip(xx,yy)
+    codes = np.ones(len(xx))*Path.LINETO
+    codes[0] = Path.MOVETO
+    codes[-1] = Path.STOP
+    print(codes)
+    path1 = Path(z1,codes)
+    patch1 = mpatches.PathPatch(path1, facecolor='r', alpha=0.5)
+    ax.add_patch(patch1)
+    if path1.contains_points([coor]):
         txt = "in geo"
     else:
         txt = "not in geo"
-    plt.plot(x1,y1,x2,y2)
-    plt.scatter(x,y,s=100)
     plt.text(100, 100,txt, color='green', fontsize=15)
-    z1 = zip(x1,y1)
-    z2 = zip(x2,y2)
-    path1 = mplPath.Path(z1)
-    patch1 = mpatches.PathPatch(path1, facecolor='r', alpha=0.5)
-    ax.add_patch(patch1)
     plt.show()
 
 
