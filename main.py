af424690d0d9e8164880902a6fbb5a1fbacc1836
mshankla <mshankla@gmail.com>
2017-02-20 20:03:49 -0600

    ci

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 import helper as hp
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.path as mplPath
+import matplotlib.patches as mpatches
 
 def inSpiral(coor):
     xp,yp = coor
@@ -10,10 +12,13 @@ def inSpiral(coor):
     y1 = 30*np.exp(0.14*t)*np.sin(t) + 15
     x2 = 18*np.exp(0.15*t)*np.cos(t) - 7
     y2 = 18*np.exp(0.15*t)*np.sin(t) + 12
+    z1 = zip(x1,y1)
+    z2 = zip(x2,y2)
     return False
 
 #define geometry
 def drawSpiral(coor):
+    fig, ax = plt.subplots()
     x,y = coor
     t = np.arange(-0.3*np.pi,3.9*np.pi,np.pi/20)
     x1 = 30*np.exp(0.14*t)*np.cos(t) - 9
@@ -27,6 +32,11 @@ def drawSpiral(coor):
     plt.plot(x1,y1,x2,y2)
     plt.scatter(x,y,s=100)
     plt.text(100, 100,txt, color='green', fontsize=15)
+    z1 = zip(x1,y1)
+    z2 = zip(x2,y2)
+    path1 = mplPath.Path(z1)
+    patch1 = mpatches.PathPatch(path1, facecolor='r', alpha=0.5)
+    ax.add_patch(patch1)
     plt.show()
 
 
